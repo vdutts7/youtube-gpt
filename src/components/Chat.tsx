94188dee6e228ebacac06e5c220fc3709e4a379d
ViveK Dutta <vivekd@berkeley.edu>
2023-06-18 17:57:02 -0700

    sldld

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { toast } from "react-toastify";

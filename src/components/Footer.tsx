158b78ff4c35e921ed891131b2411bc892c55b87
Vivek <63992417+vdutts7@users.noreply.github.com>
2023-06-20 08:21:01 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bottom-0 my-auto w-full bg-black p-6 text-white">
+    <div className="bottom-0 my-auto text-xs w-full bg-black p-6 text-white">
       <p className="text-center">
-        Data processing, backend, & UI made by 
+        Scripts, backend, UI made by 
         <span className="ml-1">
           <a
             href="https://twitter.com/vdutts7"
@@ -13,12 +13,11 @@ const Footer = () => {
             rel="noopener noreferrer"
             className="text-red-600 hover:font-bold"
           >
-            @vdutts7
+            @vdutts7.
           </a>
-        </span>{" "}
-        . Access the source code . 
-        <a href="https://github.com/vdutts7/yt-ai-chat" target="_blank" rel="noopener noreferrer" className="text-red-600 
-          hover:font-bold" >here.</a>
+          Access the source code
+          <a href="https://github.com/vdutts7/yt-ai-chat" target="_blank" rel="noopener noreferrer" className="text-red-600 
+          hover:font-bold"> here.</a>
       </p>
     </div>
   );
